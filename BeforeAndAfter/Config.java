70d4f9d0fbcb9d7364707e67e781d9ef5220af0b
SAHIL PAL <69889824+sahil-pal@users.noreply.github.com>
2021-03-11 10:55:33 +0530

    @Before and @After implementation on Aspects

---
+++ BeforeAndAfter/Config.java
@@ -0,0 +1,12 @@
+package BeforeAndAfter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("BeforeAndAfter")
+public class Config {
+
+}
